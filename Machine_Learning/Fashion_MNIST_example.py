e6df646c2866d69f02d195c80f06999e2f5763d9
lonelyaustralis <38466423+lonelyaustralis@users.noreply.github.com>
2022-02-28 10:11:34 +0800

    Add files via upload

--- Machine_Learning/Fashion_MNIST_example.py
+++ Machine_Learning/Fashion_MNIST_example.py
@@ -20,22 +20,20 @@ train_dataset = fashion_mnist_dataset(path="./data/fashion", kind="train")
 test_dataset = fashion_mnist_dataset(path="./data/fashion", kind="test")
 train_dataloader = Dataloader(train_dataset, batch_size=128, shuffle=True)
 test_dataloader = Dataloader(test_dataset, batch_size=10000, shuffle=False)
-epoch_num = 10
-
 Linear_1 = Linear(784, 256, biases=True, normalization=False)
 Linear_2 = Linear(256, 10, biases=True, normalization=False)
 # Linear_3 = Linear(50, 3, biases=True, normalization=False)
 # Linear_4 = Linear(3, 10, biases=True, normalization=False)
 model = Sequential([Linear_1, Sigmoid(), Linear_2, CE_loss_with_softmax()])
 # model = Sequential([Linear_1, Relu(), Linear_2, Relu(), Linear_3, Relu(), Linear_4, CE_loss_with_softmax()])
-Animeor = Anime_plot(
-    x_lim=[0, epoch_num],
-    y_lim=[0, 1],
-    data_x_start_point=[0] * 2,
-    data_y_start_point=[0] * 2,
-    legend=["loss", "test_acc"],
-)
+
+
+# # 载入权重
+# model.load_weights("./weight", "Fashion_MNIST_softmax_1x")
 learning_rate = 0.5
+epoch_num = 12
+Animeor = Anime_plot(x_lim=[0, epoch_num], y_lim=[0, 1], data_x_start_point=[0] * 2, data_y_start_point=[0] * 2, legend=["loss", "test_acc"],)
+
 for epoch in range(epoch_num):
     for index, (img, label) in enumerate(train_dataloader):
         lr = warm_up_lr(len(label), model.step, warm_up_steps=2000)
@@ -47,4 +45,7 @@ for epoch in range(epoch_num):
         _, _, acc, _, _ = model.eval(img, one_hot_label(label))
         acc = acc / 100
     Animeor.update([loss, acc])
+# 保存权重
+model.save_weights("./weight", "Fashion_MNIST_softmax_1x")
 Animeor.end()
+
